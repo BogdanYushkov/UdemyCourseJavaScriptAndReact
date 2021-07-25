abc712b8ee84d82271419bcad89f270e33f0b7a4
Bogdan Yushkov <bogdanyushkov8@gmail.com>
2021-07-25 18:06:57 +0300

    add fourth practice

--- test/script.js
+++ test/script.js
@@ -1,1 +1,14 @@
-"use strict";
+const car = {
+    engine: "motor",
+    wheel: "rhombus",
+    drive: function() {
+        console.log("gogogogo");
+    } 
+}
+
+console.log(car);
+
+const audi =  Object.create(car);
+
+console.log(audi.engine);
+ 
